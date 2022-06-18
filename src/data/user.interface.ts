8c9a6a9eb424db0335262329a2b9ee7e7ecaa303
Tatsiana Slapik <slapik.tatiana@gmail.com>
2022-06-18 17:38:49 +0300

    feat: add user interfase and data base users

---
+++ src/data/user.interface.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  id: string;
+  name: string;
+  age: number;
+  hobbies: String[] | [];
+}
+export interface IUserBody {
+  name: string;
+  age: number;
+  hobbies: String[] | [];
+}
