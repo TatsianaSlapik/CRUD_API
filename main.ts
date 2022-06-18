ec464c54a0024222f37981f763e42a4a7ec02c47
Tatsiana Slapik <slapik.tatiana@gmail.com>
2022-06-19 01:32:23 +0300

    feat: add all  request

--- main.ts
+++ main.ts
@@ -1,17 +1,142 @@
-import { createServer, IncomingMessage, ServerResponse } from "http";
-
-const port = 3000;
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
+import { PORT } from "./src/common/config";
+import { IUserBody } from "./src/data/user.interface";
+import { users } from "./src/data/users";
+import {
+  createUser,
+  deleteUser,
+  getId,
+  getUser,
+  isValidId,
+  updateUser,
+} from "./src/until/until";
 
 const server = createServer(
   (request: IncomingMessage, response: ServerResponse) => {
-    switch (request.url) {
-      case "/api/users": {
+    if (request.url !== undefined) {
+      if (request.url === "/api/users") {
         if (request.method === "GET") {
-          response.end(JSON.stringify({ prop: "propv" }));
+          response.end(JSON.stringify(users));
         }
-        break;
-      }
-      default: {
+        if (request.method === "POST") {
+          let body = "";
+          request.on("data", function (data) {
+            body += data;
+          });
+          request.on("end", function () {
+            try {
+              let post = createUser(JSON.parse(body));
+              response.writeHead(201, { "Content-Type": "text/plain" });
+              response.end(JSON.stringify(post));
+
+              return;
+            } catch (err) {
+              response.writeHead(500, { "Content-Type": "text/plain" });
+              response.write("Bad Post Data.  Is your data a proper JSON?\n");
+              response.end();
+              return;
+            }
+          });
+        }
+      } else if (request.url.match(/\/api\/users\/\w+/)) {
+        if (request.method === "GET") {
+          const id = getId(request.url);
+
+          try {
+            if (isValidId(id)) {
+              const user = getUser(id);
+
+              if (!user) {
+                response.writeHead(404, { "Content-Type": "application/json" });
+                response.end(JSON.stringify({ message: "User Not Found" }));
+              } else {
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.end(JSON.stringify(user));
+              }
+            } else {
+              response.writeHead(400, { "Content-Type": "application/json" });
+              response.end(
+                JSON.stringify({ message: "UserId is invalid (not uuid)" })
+              );
+            }
+          } catch (err) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write("Bad Post Data.  Is your data a proper JSON?\n");
+            response.end();
+            return;
+          }
+        }
+        if (request.method === "DELETE") {
+          const id = getId(request.url);
+
+          try {
+            if (isValidId(id)) {
+              const user = getUser(id);
+              if (!user) {
+                response.writeHead(404, { "Content-Type": "application/json" });
+                response.end(JSON.stringify({ message: "User Not Found" }));
+              } else {
+                deleteUser(id);
+                response.writeHead(204, { "Content-Type": "application/json" });
+                response.end(JSON.stringify({ message: `User ${id} removed` }));
+              }
+            } else {
+              response.writeHead(400, { "Content-Type": "application/json" });
+              response.end(
+                JSON.stringify({ message: "UserId is invalid (not uuid)" })
+              );
+            }
+          } catch (err) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write("Bad Post Data.  Is your data a proper JSON?\n");
+            response.end();
+            return;
+          }
+        }
+        if (request.method === "PUT") {
+          const id = getId(request.url);
+          let body = "";
+          request.on("data", function (data) {
+            body += data;
+          });
+          request.on("end", function () {
+            try {
+              if (isValidId(id)) {
+                const user = getUser(id);
+                if (!user) {
+                  response.writeHead(404, {
+                    "Content-Type": "application/json",
+                  });
+                  response.end(JSON.stringify({ message: "User Not Found" }));
+                } else {
+                  const { name, age, hobbies } = JSON.parse(body);
+
+                  const newInfoAboutUser: IUserBody = {
+                    name: name || user.name,
+                    age: age || user.age,
+                    hobbies: hobbies || user.hobbies,
+                  };
+                  let updateInfoUser = updateUser(id, newInfoAboutUser);
+                  response.writeHead(200, {
+                    "Content-Type": "application/json",
+                  });
+                  response.end(JSON.stringify(updateInfoUser));
+                }
+              } else {
+                response.writeHead(400, { "Content-Type": "application/json" });
+                response.end(
+                  JSON.stringify({ message: "UserId is invalid (not uuid)" })
+                );
+              }
+            } catch (err) {
+              response.writeHead(500, { "Content-Type": "text/plain" });
+              response.write("Bad Post Data.  Is your data a proper JSON?\n");
+              response.end();
+              return;
+            }
+          });
+        }
+      } else {
         response.statusCode = 404;
         response.end();
       }
@@ -19,4 +144,6 @@ const server = createServer(
   }
 );
 
-server.listen(port);
+server.listen(PORT, () =>
+  process.stdout.write(`Server is running on http://localhost:${PORT}\n`)
+);
--- src/until/until.ts
+++ src/until/until.ts
@@ -1,9 +1,21 @@
-import { v4 as uuid } from "uuid";
+import { getDiffieHellman } from "node:crypto";
+import { v4 as uuid, validate as uuidValidate } from "uuid";
 import { IUser, IUserBody } from "../data/user.interface";
 import { users } from "../data/users";
 
+export const getId = (path: string) => {
+  let mas = path.split("/");
+  return mas[mas.length - 1];
+};
+
+export const isValidId = (id: string) => {
+  return uuidValidate(id);
+};
+
 export const createUser = (user: IUserBody) => {
-  return users.push({ id: uuid(), ...user });
+  let newUser = { id: uuid(), ...user };
+  users.push(newUser);
+  return getUser(newUser.id);
 };
 
 export const getUser = (userId: string) => {
@@ -11,11 +23,11 @@ export const getUser = (userId: string) => {
 };
 
 export const deleteUser = (userId: string) => {
-  return users.filter((user) => user.id !== userId);
+  users.filter((user) => user.id !== userId);
 };
 
-export const updateUser = (userId: string, user: IUser) => {
-  const oldUser = getUser(userId);
-  const id = users.indexOf(oldUser);
-  users[id] = { ...user };
+export const updateUser = (userId: string, user: IUserBody) => {
+  const id = users.findIndex((user) => user.id === userId);
+  users[id] = { id: userId, ...user };
+  return getUser(userId);
 };
