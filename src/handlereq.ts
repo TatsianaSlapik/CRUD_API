6172ad72a7e07f97eb8c8ce906583ee94d2ceae3
Tatsiana Slapik <slapik.tatiana@gmail.com>
2022-06-19 15:03:56 +0300

    feat: implement horizontal scaling for application

---
+++ src/handlereq.ts
@@ -0,0 +1,164 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { IUserBody } from "./data/user.interface";
+import users from "./data/users";
+import {
+  createUser,
+  deleteUser,
+  getId,
+  getUser,
+  isValidId,
+  updateUser,
+} from "./util/util";
+
+export function handlereqres(
+  request: IncomingMessage,
+  response: ServerResponse
+) {
+  if (request.url !== undefined) {
+    if (request.url === "/api/users") {
+      if (request.method === "GET") {
+        response.end(JSON.stringify(users));
+      }
+      if (request.method === "POST") {
+        let body = "";
+        request.on("data", function (data) {
+          body += data;
+        });
+        request.on("end", function () {
+          try {
+            const { name, age, hobbies } = JSON.parse(body);
+
+            const newUser: IUserBody = {
+              name: name,
+              age: age,
+              hobbies: hobbies,
+            };
+            if (newUser.name && newUser.age && newUser.hobbies) {
+              let post = createUser(newUser);
+              response.writeHead(201, { "Content-Type": "text/plain" });
+              response.end(JSON.stringify(post));
+            } else {
+              response.writeHead(400, { "Content-Type": "application/json" });
+              response.end(
+                JSON.stringify({
+                  message: "Request body does not contain required fields",
+                })
+              );
+            }
+
+            return;
+          } catch (err) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write("Bad Post Data.  Is your data a proper JSON?\n");
+            response.end();
+            return;
+          }
+        });
+      }
+    } else if (request.url.match(/\/api\/users\/\w+/)) {
+      if (request.method === "GET") {
+        const id = getId(request.url);
+
+        try {
+          if (isValidId(id)) {
+            const user = getUser(id);
+
+            if (!user) {
+              response.writeHead(404, { "Content-Type": "application/json" });
+              response.end(JSON.stringify({ message: "User Not Found" }));
+            } else {
+              response.writeHead(200, { "Content-Type": "application/json" });
+              response.end(JSON.stringify(user));
+            }
+          } else {
+            response.writeHead(400, { "Content-Type": "application/json" });
+            response.end(
+              JSON.stringify({ message: "UserId is invalid (not uuid)" })
+            );
+          }
+        } catch (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.write("Bad Post Data.  Is your data a proper JSON?\n");
+          response.end();
+          return;
+        }
+      }
+      if (request.method === "DELETE") {
+        const id = getId(request.url);
+
+        try {
+          if (isValidId(id)) {
+            const user = getUser(id);
+            if (!user) {
+              response.writeHead(404, { "Content-Type": "application/json" });
+              response.end(JSON.stringify({ message: "User Not Found" }));
+            } else {
+              deleteUser(id);
+              response.writeHead(204, { "Content-Type": "text/plain" });
+              response.end(JSON.stringify({ message: `User ${id} removed` }));
+            }
+          } else {
+            response.writeHead(400, { "Content-Type": "application/json" });
+            response.end(
+              JSON.stringify({ message: "UserId is invalid (not uuid)" })
+            );
+          }
+        } catch (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.write("Bad Post Data.  Is your data a proper JSON?\n");
+          response.end();
+          return;
+        }
+      }
+      if (request.method === "PUT") {
+        const id = getId(request.url);
+        let body = "";
+        request.on("data", function (data) {
+          body += data;
+        });
+        request.on("end", function () {
+          try {
+            if (isValidId(id)) {
+              const user = getUser(id);
+              if (!user) {
+                response.writeHead(404, {
+                  "Content-Type": "application/json",
+                });
+                response.end(JSON.stringify({ message: "User Not Found" }));
+              } else {
+                const { name, age, hobbies } = JSON.parse(body);
+
+                const newInfoAboutUser: IUserBody = {
+                  name: name || user.name,
+                  age: age || user.age,
+                  hobbies: hobbies || user.hobbies,
+                };
+                let updateInfoUser = updateUser(id, newInfoAboutUser);
+                response.writeHead(200, {
+                  "Content-Type": "application/json",
+                });
+                response.end(JSON.stringify(updateInfoUser));
+              }
+            } else {
+              response.writeHead(400, { "Content-Type": "application/json" });
+              response.end(
+                JSON.stringify({ message: "UserId is invalid (not uuid)" })
+              );
+            }
+          } catch (err) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write("Bad Post Data.  Is your data a proper JSON?\n");
+            response.end();
+            return;
+          }
+        });
+      }
+    } else {
+      response.statusCode = 404;
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("OOps");
+      response.end();
+      response.end();
+    }
+  }
+}
---
+++ src/mainCluster.ts
@@ -0,0 +1,27 @@
+import cluster from "node:cluster";
+import http from "node:http";
+import { cpus } from "node:os";
+import process from "node:process";
+import { PORT } from "./common/config";
+import { handlereqres } from "./handlereq";
+
+const numCPUs = cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer(handlereqres).listen(PORT);
+
+  console.log(`Worker ${process.pid} started`);
+}
